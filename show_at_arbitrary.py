6a20198ed2040b063e491b4b145502bd300f2f17
hbaughman <hbaughman741@gmail.com>
2014-08-05 15:22:33 -0400

    Rename variables and method names to reflect new use

--- show_at_arbitrary.py
+++ show_at_arbitrary.py
@@ -12,18 +12,18 @@ class CaretWatcher(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
         # caret has moved, reset positions to their default value
         global positions
-        positions = cycle(settings().get('recenter_positions'))
+        positions = cycle(settings().get('target_lines'))
 
 
 class ShowAtArbitraryCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global positions
-        positions = positions or cycle(settings().get('recenter_positions'))
-        posn = next(positions)
-        if posn > 0:
-            self.show_at_top(posn)
-        elif posn < 0:
-            self.show_at_bottom(abs(posn))
+        positions = positions or cycle(settings().get('target_lines'))
+        target_line = next(positions)
+        if target_line > 0:
+            self.show_from_top(target_line)
+        elif target_line < 0:
+            self.show_from_bottom(abs(target_line))
         else:
             self.view.run_command('show_at_center')
 
@@ -31,12 +31,12 @@ class ShowAtArbitraryCommand(sublime_plugin.TextCommand):
         caret = self.view.sel()[0].begin()
         return self.view.rowcol(caret)[0]
 
-    def show_at_top(self, target_line):
+    def show_from_top(self, target_line):
         top, _ = self.screen_extents()
         offset = self.row() - top - target_line
         self.view.run_command('scroll_lines', {'amount': -offset})
 
-    def show_at_bottom(self, target_line):
+    def show_from_bottom(self, target_line):
         _, bottom = self.screen_extents()
         offset = bottom - self.row() - target_line
         self.view.run_command('scroll_lines', {'amount': offset})
